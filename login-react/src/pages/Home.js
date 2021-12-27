552146165f7d59d12f95ef27503cd8d8935db4f6
Angel Morales R <kisamenarusas@gmail.com>
2021-12-27 09:10:00 -0600

    strapi config

--- login-react/src/pages/Home.js
+++ login-react/src/pages/Home.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const providersNames = [
-  'discord',
-  'facebook',
-  'github',
-  'google',
-  'instagram',
-  'linkedin',
-  'reddit',
-  'twitch',
-  'twitter',
-  'vk',
-  'auth0',
+  "discord",
+  "facebook",
+  "github",
+  "microsoft",
+  "instagram",
+  "linkedin",
+  "reddit",
+  "twitch",
+  "twitter",
+  "vk",
+  "auth0",
 ];
 
-const LoginButton = (props) => <a href={`${backendUrl}/api/connect/${props.providerName}`}>
-    <button style={{ width: '150px' }}>Connect to {props.providerName}</button>
-  </a>;
+const LoginButton = (props) => (
+  <a href={`${backendUrl}/connect/${props.providerName}`}>
+    <button style={{ width: "150px" }}>Connect to {props.providerName}</button>
+  </a>
+);
 
 const LogoutButton = (props) => <button onClick={props.onClick}>Logout</button>;
 
 const Home = (props) => {
-  const [isLogged, setIsLogged] = useState(!!localStorage.getItem('jwt'));
+  const [isLogged, setIsLogged] = useState(!!localStorage.getItem("jwt"));
 
   const logout = (e) => {
     e.preventDefault();
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('username');
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("username");
     setIsLogged(false);
   };
 
@@ -37,25 +39,31 @@ const Home = (props) => {
   if (isLogged) {
     buttons = <LogoutButton onClick={logout} />;
   } else {
-    buttons = <ul style={{ listStyleType: 'none' }}>
-      {providersNames.map((providerName, i) => <li key={providerName}>
-        <LoginButton providerName={providerName}/>
-        </li>)}
-    </ul>;
+    buttons = (
+      <ul style={{ listStyleType: "none" }}>
+        {providersNames.map((providerName, i) => (
+          <li key={providerName}>
+            <LoginButton providerName={providerName} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   let text;
 
   if (isLogged) {
-    text = `Welcome ${localStorage.getItem('username')}, you are connected!`;
+    text = `Welcome ${localStorage.getItem("username")}, you are connected!`;
   } else {
-    text = 'You are not connected. Please log in.';
+    text = "You are not connected. Please log in.";
   }
 
-  return <div>
-    <p>{text}</p>
-    {buttons}
-  </div>;
-}
+  return (
+    <div>
+      <p>{text}</p>
+      {buttons}
+    </div>
+  );
+};
 
 export default Home;
